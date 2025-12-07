7aeeb0e16ddcc514218ed95e494b5097e4ace684
Hailey Zweedyk <haileyzweedyk@Haileys-MBP-2.lan>
2025-12-06 21:59:50 -0500

    Yup still here

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -20,7 +20,7 @@
         style="cursor: pointer;"
       >
         <img :src="product.image" :alt="product.name" />
-        <p class="product-path">test one two three four{{  product.image }}</p>
+        <p class="product-path">test one two three{{  product.image }}</p>
         <p class="product-name">{{ product.name }}</p>
       </div>
     </div>
